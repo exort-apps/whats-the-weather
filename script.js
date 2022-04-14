20618bfcbadc6f6158dcd2bd1a37a996f99c96f5
exort-apps <67579080+exort-apps@users.noreply.github.com>
2022-04-14 22:04:24 +0200

    changing background based on chosen city

--- script.js
+++ script.js
@@ -15,8 +15,32 @@ let weather = {
         const { icon, description } = data.weather[0]
         const { temp, humidity } = data.main
         const { speed } = data.wind
-        console.log(name, icon, description, temp, humidity, speed)
         document.querySelector(".city").innerText = "Weather in " + name
-
+        document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + ".png"
+        document.querySelector(".description").innerText = description
+        document.querySelector(".temperature").innerText = temp + " °C"
+        document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%"
+        document.querySelector(".wind").innerText = "Wind speed of: " + speed + " km/h"
+        document.querySelector(".weather").classList.remove("loading")
+        document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?" + name + "')"
+    },
+    search: function () {
+        this.fetchWeather(document.querySelector(".search-bar").value)
     }
 }
+
+
+document.querySelector(".search button").addEventListener("click", function () {
+    weather.search()
+}
+)
+document.querySelector(".search-bar").addEventListener("keyup", function (event) {
+    if (event.key == "Enter") {
+        weather.search()
+    }
+})
+
+// this calls the function to fetch a weather result as soon as the page loads with a default city
+// in plan to use a default city based on user location
+// currently set a default city of Belgrade
+weather.fetchWeather("Belgrade")
